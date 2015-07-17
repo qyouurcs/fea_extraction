4582dd947c21229798a53efbc37e6a48509f0ee2
Quanzeng You <qyou@cs.rochester.edu>
2015-07-17 19:26:02 -0400

    update for calculate distance two different hdf5 directories

--- run_calc_dist.sh
+++ run_calc_dist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-    echo "Usage: $0 <h5py_dir> <process_id> <total_processes> [fea_len=1024]"
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 <h5py_dir_1mlillion> <h5py_dir_target> <process_id> <total_processes> [fea_len=1024]"
     exit
 fi
 
@@ -19,12 +19,13 @@ local_lib="/mnt/ilcompf2d0/project/qyou/libs/"
 # this is really stupid. however, just use it currently.
 
 h5py_dir=$1
-process_id=$2
-total_process=$3
+h5py_dir_dst=$2
+process_id=$3
+total_process=$4
 fea_len=1024
 
-if [ $# -ge 4 ]; then
-    fea_len=$4
+if [ $# -ge 5 ]; then
+    fea_len=$5
 fi
 
 if [ -z $LD_LIBRARY_PATH ]; then
@@ -41,4 +42,4 @@ else
 fi
 
 echo $PYTHONPATH
-python script_calc_dist.py $fea_dir $process_id $total_process $fea_len
+python script_calc_dist.py $h5py_dir $h5py_dir_dst $process_id $total_process $fea_len
--- script_calc_dist.py
+++ script_calc_dist.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy.spatial.distance as dist
 import pdb
 
-def distance(batch_fn, dummy_matrix, dummy_fns, save_dir, process_id, top=50):
+def distance(batch_fn, dummy_matrix, dummy_fns, save_dir, process_id, top=50, save_fns = False):
 
     '''This funciton calculate the distance between the current batch with all the batches in the fea dir.
     '''
@@ -25,10 +25,11 @@ def distance(batch_fn, dummy_matrix, dummy_fns, save_dir, process_id, top=50):
 
     dist_inc = None
     save_fn = os.path.basename(batch_fn)
-    save_fn = os.path.join(save_dir, save_fn)
+    save_fn = os.path.join(save_dir, str(save_fns) + '_' + str(process_id) + '_' + save_fn)
     f = h5py.File(save_fn, 'w')
     f.create_dataset('dist', data = d)
-    f.create_dataset('fns', data = dummy_fns)
+    if save_fns: # all the same process share the same copy of the fns.
+        f.create_dataset('fns', data = dummy_fns)
     f.close()
     batch.close()
     print >> sys.stderr, 'Process ', process_id, 'done with', batch_fn
@@ -44,20 +45,23 @@ def total_images(h5py_dir):
     return total_imgs
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print 'Usage: {0} <h5py_dir> <process_id> <total_processes> [fea_len=1024]'.format(sys.argv[0])
+
+    if len(sys.argv) < 5:
+        print 'Usage: {0} <h5py_dir_1million> <h5py_dir_target> <process_id> <total_processes> [fea_len=1024]'.format(sys.argv[0])
         sys.exit()
 
     h5py_dir = sys.argv[1]
-    process_id = int(sys.argv[2])
-    process_num = int(sys.argv[3])
+    h5py_dir_dst = sys.argv[2]
+
+    process_id = int(sys.argv[3])
+    process_num = int(sys.argv[4])
     fea_len = 1024
-    if len(sys.argv) >= 5:
-        fea_len = int(sys.argv[4])
+    if len(sys.argv) >= 6:
+        fea_len = int(sys.argv[5])
     
-    if h5py_dir[-1] =='/':
-        h5py_dir = h5py_dir[0:-1]
-    save_dir = h5py_dir + '_dist'
+    if h5py_dir_dst[-1] =='/':
+        h5py_dir_dst = h5py_dir_dst[0:-1]
+    save_dir = h5py_dir_dst + '_dist'
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
 
@@ -79,12 +83,13 @@ if __name__ == '__main__':
                 dummy_fns.append(fns[i])
             batch.close()
             fea_idx += fea.shape[0]
-
-    for root, sub_dir, fns in os.walk(h5py_dir):
+    save_fns = True
+    for root, sub_dir, fns in os.walk(h5py_dir_dst):
         for fn in fns:
             ifn = os.path.splitext(fn)[0]
             idx = int(ifn.split('_')[-1])
             if not idx % process_num:
                 # it's my job to do it. 
                 full_fn = os.path.join(root, fn)
-                distance(full_fn, dummy_fea, dummy_fns, save_dir, process_id)
+                distance(full_fn, dummy_fea, dummy_fns, save_dir, process_id, save_fns=save_fns)
+                save_fns = False
