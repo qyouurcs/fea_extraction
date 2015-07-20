44267ab43d3354e0546d69da7018b1b1fd6daa01
qyouurcs <qyou@cs.rochester.edu>
2015-07-20 08:44:31 -0700

    update

---
+++ run_check_true_h5py.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import h5py
+import sys
+import pdb
+import glob
+
+if __name__ == '__main__':
+    # check the consistency between different h5py file. 
+    if len(sys.argv) < 2:
+        print 'Usage: {0} <h5py.dir>'.format(sys.argv[0])
+        sys.exit()
+    src_dir = sys.argv[1]
+    h5pys = glob.glob( os.path.join(src_dir, 'True*'))
+
+    for fn in h5pys:
+        fn = h5py.File(sys.argv[1],'r')
+
+        pdb.set_trace()
+        print fn.keys()
+
+
